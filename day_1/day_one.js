26c7100cb7491ccbd53d8b3d0dc541b45c76acf8
james <jamesfikrii@gmail.com>
2020-01-21 09:36:38 +0700

    day_2

---
+++ day_1/day_one.js
@@ -0,0 +1,79 @@
+// VARIABEL & TYPE DATA & COMMENT
+
+// VARIABEL = WADAH YANG PUNYA NAMA
+// declaration
+// var kelas;
+
+// // assignment
+// kelas = 'Purwadhika'
+
+// // declare + assignment
+// var kelas = 'Purwadhika'
+
+
+
+// kelas = 'Web Mobile'
+// var kelas = 'Data Science'
+
+// console.log(kelas)
+
+// // integer, string, boolean
+
+// var nama = 'fikri123' // string , varchar
+// var num = 1029382 // integer , number
+// var bol = true // boolean
+// var siapa = nama
+
+
+
+// console.log(kelas , siapa)
+
+// UNDO + REDO
+// ctrl +z , ctrl + y
+
+
+
+
+
+
+
+// VAR vs LET
+
+
+// var nama = 'fikri'
+// var nama = 'siapa'
+
+// console.log(nama)
+
+// let kelas = 'purwadhika'
+// kelas = 'web mobile'
+
+// console.log(kelas)
+
+
+// PENGKONDISIAN / FLOWCONTROL 
+
+// if(condition){
+//     //action
+// }
+
+
+let number = 11
+if(number >10){
+    console.log('Besar')
+}else{
+    console.log('Kecil')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
