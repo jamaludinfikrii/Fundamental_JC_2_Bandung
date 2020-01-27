152db44e032adf5a74dc679f415b37ad8f9cf285
james <jamesfikrii@gmail.com>
2020-01-27 11:39:50 +0700

    data murid app

---
+++ day_6/array.js
@@ -0,0 +1,53 @@
+var mobil_1 = 'Avanza'
+var harga_mobil_1 = '100jt'
+var mobil_2 = 'Xenia'
+var harga_mobil_2 = '90jt'
+var mobil_3 = 'Alya'
+var harga_mobil_3 = '120jt'
+
+
+var print = 
+`Daftar Mobil
+1. ${mobil_1} , Rp. ${harga_mobil_1}
+2. ${mobil_2} , Rp. ${harga_mobil_2}
+3. ${mobil_3} , Rp. ${harga_mobil_3}`
+
+
+var mobil = ['Avanza' , 'Xenia','Alya']
+var harga = [100,90,120]
+
+var print = 'Daftar Mobil\n'
+for(var i = 0 ; i < mobil.length ; i ++){
+    print += mobil[i] + ', Rp. ' + harga[i] + 'jt\n'
+}
+
+// console.log(print)
+
+
+// var print = 
+// `Daftar Mobil
+// 1. ${mobil[0]} , Rp . ${harga[0]}jt
+// 2. ${mobil[1]} , Rp . ${harga[1]}jt
+// 3. ${mobil[2]} , Rp . ${harga[2]}jt
+
+// `
+
+
+
+
+
+
+
+
+
+
+
+
+// POP & PUSH
+
+var mobil = ['Avanza' , 'Xenia','Alya']
+mobil.pop()
+mobil.push('Alphard')
+console.log(mobil)
+
+mobil.splice(0,2)
